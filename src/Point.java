2c2536ed7a3371feaba7e81f280e1a0c711bd861
Master <charleshuang091@gmail.com>
2025-04-22 00:39:35 -0400

    Revised code for assignment 9

--- src/Point.java
+++ src/Point.java
@@ -1,7 +1,12 @@
 public class Point {
-   
+
     private double x;
     private double y;
+
+    public Point() {
+        this.x = 0;
+        this.y = 0;
+    }
 
     public Point(double x, double y) {
         this.x = x;
@@ -9,45 +14,51 @@ public class Point {
     }
 
     public Point difference(Point point) {
-        Point p1 = new Point(x,y);
-        Point p2 = new Point(x,y);
-        Point p3 = p1.difference(p2);
-        return p3;
+        return new Point(this.x - point.getX(), this.y - point.getY());
     }
 
     public void displayCoordinate() {
-        
+        System.out.println("The x-coordinate is: " + x);
+        System.out.println("The y-coordinate is: " + y);
     }
 
     public double distance(Point point) {
-
+        double distanceX = this.x - point.getX();
+        double distanceY = this.y - point.getY();
+        return Math.sqrt(distanceX * distanceX + distanceY * distanceY);
     }
 
     public double getX() {
-
+        return x;
     }
 
     public double getY() {
-
+        return y;
     }
 
     public void linearEquation(Point point) {
-
+        double m = slope(point);
+        double b = yIntercept(point);
+        System.out.println("y = " + m + "x + " + b);
     }
 
     public Point midpoint(Point point) {
-
+        return new Point((this.x + point.getX()) / 2, (this.y + point.getY()) / 2);
     }
 
     public double slope(Point point) {
-
+        if (this.x == point.getX()) {
+            System.out.println("Undefined slope");
+        }
+        return (this.y - point.getY()) / (this.x - point.getX());
     }
 
     public Point sum(Point point) {
-
+        return new Point(this.x + point.getX(), this.y + point.getY());
     }
 
     public double yIntercept(Point point) {
-
+        double m = slope(point);
+        return this.y - m * this.x;
     }
 }
